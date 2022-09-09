6ee13c2b5976ea59ddb0239d77277f60a59e3865
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-09 16:23:26 +0300

    add main javascript action

---
+++ index.js
@@ -0,0 +1,8 @@
+const genderURL = "https://api.genderize.io?name=";
+const ageURL = " https://api.agify.io/?name=";
+const nationalityURL =  "https://api.nationalize.io/?name=";
+const dogUrl = "https://dog.ceo/api/breeds/image/random";
+
+document.getElementById('submit').onclick = function () {
+    const name = document.getElementById('name').value ;
+}
